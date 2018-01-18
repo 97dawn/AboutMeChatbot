e1fbcffa9d6a138d22cc72f81b30997c46d6f3c0
Doeun Kim <graceee318@gmail.com>
2018-01-19 00:20:59 +0900

    done with showing the celebrity bio

--- src/aboutmechatbot/Data.java
+++ src/aboutmechatbot/Data.java
@@ -15,34 +15,34 @@ public class Data {
          celebrities = new HashMap<>();
          //  name, image, job,birth, height,weight,youtube, instagram, twitter
          String[] MinhoLee = {"Minho Lee",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201605%2F20160517121435641-157383.jpg",
+         "file:images/MinhoLee.jpg",
          "Actor","1987.6.22","187cm","71kg","https://www.youtube.com/results?search_query=%EC%9D%B4%EB%AF%BC%ED%98%B8", "https://www.instagram.com/Actorleeminho/",null};
          String[] JihyunJeon = {"Jihyun Jeon",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F125%2F201212262021589401.jpg",
+         "file:images/JihyunJeon.jpg",
          "Actress","1981.10.30","173.5cm", "52kg","https://www.youtube.com/results?search_query=%EC%A0%84%EC%A7%80%ED%98%84", null, null};
          String[] Jisung = {"Taegeun Kwak",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F5%2F201308061746201891.jpg",
+         "file:images/Jisung.jpg",
          "Actor","1977.2.27","178cm", "70kg","https://www.youtube.com/results?search_query=%EC%A7%80%EC%84%B1","https://www.instagram.com/justin_jisung/",null};
          String[] BoyoungLee = {"Boyoung Lee",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201702%2F20170203185134627-750608.jpg",
+         "file:images/BoyoungLee.jpg",
          "Actress","1979.1.12","168cm",null,"https://www.youtube.com/results?search_query=%EC%9D%B4%EB%B3%B4%EC%98%81",null,null};
          String[] Zico = {"Jiho Woo",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201801%2F20180108113919887.jpg",
+         "file:images/JihoWoo.jpg",
          "Rapper in BlockB","1992.9.14", "182cm", "65kg","https://www.youtube.com/results?search_query=%EC%A7%80%EC%BD%94" ,"https://www.instagram.com/woozico0914/","https://twitter.com/ZICO92"};
          String[] GD = {"Jiyong Kwon",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F5%2F201612152122408071.jpg",
+         "file:images/JiyongKwon.jpg",
          "Rapper and Singer in BigBang","1988.8.18","177cm", "58kg","https://www.youtube.com/results?search_query=%EA%B6%8C%EC%A7%80%EC%9A%A9","https://www.instagram.com/xxxibgdrgn/","https://twitter.com/ibgdrgn"};
          String[] Sunmi = {"Sunmi Lee", 
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201708%2F20170829182154429.jpg",
+         "file:images/SunmiLee.jpg",
          "Singer","1992.5.2","166cm","43kg","https://www.youtube.com/results?search_query=%EC%84%A0%EB%AF%B8","https://www.instagram.com/miyayeah/","https://twitter.com/official_sunmi_"};
          String[] Taeyeon = {"Taeyeon Kim",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F188%2F201712121421546321.jpg",
+         "file:images/TaeyeonKim.jpg",
          "Singer in SNSD","1989.3.9",null, null, "https://www.youtube.com/results?search_query=%ED%83%9C%EC%97%B0", "https://www.instagram.com/Taeyeon_ss/",null};
          String[] YonghwaJung = {"Yonghwa Jung",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F160%2F201707131222492181.jpg",
+         "file:images/YonghwaJung.jpg",
          "Singer in CNBlue","1989. 6.22","180cm", "63kg", "https://www.youtube.com/results?search_query=%EC%A0%95%EC%9A%A9%ED%99%94","https://www.instagram.com/jyheffect0622/","https://twitter.com/JYHeffect"};
          String[] DujunYoon = {"Dujun Yoon",
-         "https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201710%2F2017101318251678.jpg",
+         "file:images/DujunYoon.jpg",
          "Sing in Highlight","1989.7.4", "178cm", "66kg", "https://www.youtube.com/results?search_query=%EC%9C%A4%EB%91%90%EC%A4%80", "https://www.instagram.com/beeeestdjdjdj/","https://twitter.com/BeeeestDJ"};
          celebrities.put("Minho Lee", MinhoLee);
          celebrities.put("Jihyun Jeon", JihyunJeon);
--- src/aboutmechatbot/FXMLDocumentController.java
+++ src/aboutmechatbot/FXMLDocumentController.java
@@ -7,8 +7,7 @@ package aboutmechatbot;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
-import com.sun.javafx.tk.FontLoader;
-import com.sun.javafx.tk.Toolkit;
+import javafx.scene.image.Image;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +17,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 /**
@@ -27,104 +27,112 @@ import javafx.scene.layout.Pane;
 public class FXMLDocumentController implements Initializable {
 
     private HashMap<String, String[]> celebrities;
-    private HashMap<String, Label> history;
-    private String[] info ;
+    private ArrayList<Label> labels;
+    private ArrayList<String> history;
+    private String[] info;
     @FXML
     private ScrollPane scroll;
     @FXML
     private JFXTextArea message;
+    @FXML
+    private JFXButton send;
     private Pane pane;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        pane=new Pane();
+        pane = new Pane();
+        history = new ArrayList<>();
         // SET CELEBRITIES' DATA
         Data data = new Data();
         celebrities = data.getCelebritiesData();
         // ADD MESSAGE HISTORY
-        history = new HashMap<>();
+        labels = new ArrayList<>();
         scroll.setContent(pane);
         Message m = new Message("Welcome. Which Korean celebrity \ndo you want to know? \n(format: FirstName LastName )");
         Label greet = m.getPCMessage(0, 0);
         pane.getChildren().add(greet);
-        history.put("PC:GREET", greet);
+        labels.add(greet);
+        history.add("GREET");
     }
     
     @FXML
     public void handleButtonAction(ActionEvent event) {
-        FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
-        if(history.keySet().toArray()[history.size()-1].equals("PC:GREET")){
+        Label prevLabel = (Label) labels.get(labels.size() - 1);
+        if (history.get(history.size() - 1).equals("GREET") || history.get(history.size() - 1).equals("REWRITE")) {
             String name = message.getText();
             // UPLOAD USER'S MESSAGE
-            Message m = new Message(name);
-            Label prevLabel = history.get("PC:GREET");
-            Label currentLabel = m.getUserMessage(scroll.getPrefWidth() -fontLoader.computeStringWidth(name, prevLabel.getFont())-10 , prevLabel.getHeight()+10);
+            Label currentLabel = new Label(name+"   ");
+            currentLabel.setWrapText(true);
+            currentLabel.setLayoutX(0);
+            currentLabel.setLayoutY( prevLabel.getLayoutY()+prevLabel.getHeight() + 10);
+            currentLabel.setStyle("-fx-background-color :  #b3ffff");
             pane.getChildren().add(currentLabel);
+            labels.add(currentLabel);
+            history.add("CELEB_NAME");
             handleName(name);
-        }
-    }
-    
-    private void handleName(String name){
-        Label pc = new Label();
-        if (name.equals("")) {
-            System.out.println("Please write a message");
         } 
-        else {
+    }
+
+    private void handleName(String name) {
+        Label prevLabel = (Label) labels.get(labels.size() - 1);
+         message.setText("");
+        if (name.equals("")) {
+            Message m = new Message("Please write a message");
+            Label pc = m.getPCMessage(0, prevLabel.getLayoutY() + prevLabel.getHeight() + 20);
+            pane.getChildren().add(pc);
+            labels.add(pc);
+            history.add("REWRITE");
+        } else {
             if (name.contains("Jihyun Jeon") || name.contains("Jihyun Jun") || name.contains("Jihyeon Jeon") || name.contains("jihyun jeon") || name.contains("jihyun jun") || name.contains("jihyeon jeon")) {
-                history.put("USER:CELEBRITY_NAME", pc);
                 info = celebrities.get("Jihyun Jeon");
-            } 
-            else if(name.contains("Minho Lee") || name.contains("minho lee")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("Minho Lee") || name.contains("minho lee")) {
                 info = celebrities.get("Minho Lee");
-            }
-            else if(name.contains("Jisung") || name.contains("jisung")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("Jisung") || name.contains("jisung")) {
                 info = celebrities.get("Taegeun Kwak");
-            }
-            else if(name.contains("Boyoung") || name.contains("boyoung")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("Boyoung") || name.contains("boyoung")) {
                 info = celebrities.get("Boyoung Lee");
-            }
-            else if(name.contains("jico") || name.contains("Zico") || name.contains("Jico") || name.contains("jiko") || name.contains("Ziko") || name.contains("Jiko")||name.contains("zico") || name.contains("ziko")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("jico") || name.contains("Zico") || name.contains("Jico") || name.contains("jiko") || name.contains("Ziko") || name.contains("Jiko") || name.contains("zico") || name.contains("ziko")) {
                 info = celebrities.get("Jiho Woo");
-            }
-            else if(name.contains("dragon") || name.contains("gd") || name.contains("GD")||name.contains("Dragon")){
-               history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("dragon") || name.contains("gd") || name.contains("GD") || name.contains("Dragon")) {
                 info = celebrities.get("Jiyong Kwon");
-            }
-            else if(name.contains("sunmi") || name.contains("Sunmi") || name.contains("Seonmi")||name.contains("seonmi")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("sunmi") || name.contains("Sunmi") || name.contains("Seonmi") || name.contains("seonmi")) {
                 info = celebrities.get("Sunmi Lee");
-            }
-            else if(name.contains("taeyeon") || name.contains("Taeyeon")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("taeyeon") || name.contains("Taeyeon")) {
                 info = celebrities.get("Taeyeon Kim");
-            }
-            else if(name.contains("yonghwa") || name.contains("Yonghwa")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("yonghwa") || name.contains("Yonghwa")) {
                 info = celebrities.get("Yonghwa Jung");
-            }
-            else if(name.contains("Dujun") || name.contains("Doojoon") || name.contains("Dujoon")||name.contains("Doojun") || name.contains("dujun") || name.contains("doojoon")||name.contains("dujoon") || name.contains("doojun")){
-                history.put("USER:CELEBRITY_NAME", pc);
+            } else if (name.contains("Dujun") || name.contains("Doojoon") || name.contains("Dujoon") || name.contains("Doojun") || name.contains("dujun") || name.contains("doojoon") || name.contains("dujoon") || name.contains("doojun")) {
                 info = celebrities.get("Dujun Yoon");
-            }            
-            else {
-                System.out.println("I don't know. Please write a name again");
+            } else if(!name.equals("")) {
+                Message m = new Message("I don't know who this person is. Please write a name again");
+                Label pc = m.getPCMessage(0, prevLabel.getLayoutY() + prevLabel.getHeight() + 20);
+                pane.getChildren().add(pc);
+                labels.add(pc);
+                history.add("REWRITE");
             }
         }
         // THERE IS A CELEBRITY IN DATA
-        if(info != null){
-            message.setText("");
+        if (info != null) {
             PCShowInfo(info);
         }
     }
-    private void PCShowInfo(String[] info){
-        Label pc = new Label();
-        for(int i = 0 ; i < info.length; i++){
-            
+    private void PCShowInfo(String[] info) {
+        try {
+            Label prevLabel = (Label) labels.get(labels.size() - 1);
+            String text = info[0] + "  \n\n" + info[2] + "  \n\n" + info[3] + "  \n\n" + info[4] + "  \n\n" + info[5];
+            Image img = new Image(info[1]);
+            ImageView image = new ImageView(img);
+
+            Label pc = new Label(text, image);
+            pc.setStyle("-fx-background-color :  #d1f1b5");
+            pc.setLayoutX(0);
+            pc.setLayoutY(prevLabel.getLayoutY() + prevLabel.getHeight() + 20);
+            pc.setWrapText(true);
+            history.add("CELEB_INFO");
+            pane.getChildren().add(pc);
+            labels.add(pc);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    
 }
